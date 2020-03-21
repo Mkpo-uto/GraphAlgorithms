06350733bf2e33cca4fa7a31941728684e485ba2
mkpou <mkpoutoe@hotmail.com>
2020-03-21 16:35:28 +1100

    breadth first search complete

--- src/WebCrawler/Application.java
+++ src/WebCrawler/Application.java
@@ -2,5 +2,15 @@ package WebCrawler;
 
 public class Application {
 
+    public static void main(String[] args){
+        WebCrawler crawler = new WebCrawler();
+        String rootUrl = "http://www.bbc.com";
+        crawler.discoverWeb(rootUrl);
+
+
+
+    }
+
+
 
 }
